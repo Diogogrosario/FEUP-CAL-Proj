5842c7b2a3d9e548cef94a933fc076bb23e25b03
Diogo Rosário <diogogrosario1008@gmail.com>
2020-05-23 21:07:21 +0100

    csv files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <graphviewer.h>
 #include <chrono>
+#include <fstream>
 #include "readFile.h"
 #include "Node.h"
 #include "Graph.h"
@@ -14,6 +15,9 @@ extern GraphViewer * gv;
 extern Graph<Node> graph;
 
 int main() {
+    std::fstream myfile;
+    myfile.open("../vehicles.csv",std::ios_base::app);
+
     initGraph();
 	readNodeFile("../nodes.txt");
 	readEdgeFile("../edges.txt");
@@ -23,13 +27,16 @@ int main() {
 	cout << "Graph is " << ret << endl;
 
 	vector<Client> clients;
-    Client c1("José",123456789,215,Time(15,30));
+    Client c1("José",123456789,215,Time(17,30));
 	Client c2("Alberto",123456789,390,Time(17,30));
     Client c3("Diogo",123456789,354,Time(17,30));
     Client c4("Davide",123456789,284,Time(17,30));
     Client c5("Eduardo",123456789,504,Time(17,30));
     Client c6("Henrique",123456789,781,Time(17,30));
     Client c7("Bastos", 123456789,508,Time(17,30));
+    Client c8("Carlos",123456789,421,Time(17,30));
+    Client c9("Bastos", 123456789,101,Time(17,30));
+    Client c10("Carlos",123456789,12,Time(17,30));
     clients.push_back(c1);
     clients.push_back(c2);
     clients.push_back(c7);
@@ -37,6 +44,9 @@ int main() {
     clients.push_back(c4);
     clients.push_back(c5);
     clients.push_back(c6);
+    clients.push_back(c8);
+    clients.push_back(c9);
+    clients.push_back(c10);
 
 
     Client admin("Admin",123456789,230,Time(15,30));
@@ -46,7 +56,14 @@ int main() {
     vehicles.push_back(v1),
     vehicles.push_back(v2);
 
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
     assignClients(vehicles, clients); //main algorithm
+
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+    myfile << vehicles.size() << "," << chrono::duration_cast<chrono::milliseconds>(end-begin).count() << "\n";
+    myfile.close();
 
     int choice = -1;
 
@@ -74,8 +91,6 @@ int main() {
                 createClient(&add);
                 clients.push_back(add);
                 break;
-            case 0:
-                gv->closeWindow();
             case 4:
                 for(Vehicle &v: vehicles){
                     v.clients.clear();
